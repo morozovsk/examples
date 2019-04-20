0ebbc446aa4f0b02c1c52fbc260ded7c691298d9
Vladimir Goncharov <morozovsk@gmail.com>
2019-04-20 22:19:26 +0300

    Create install_event_extension.sh

---
+++ php/install_event_extension.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#https://pecl.php.net/package/event
+#it needs for better performance of https://github.com/walkor/Workerman
+
+#centos
+yum install -y php-pecl-event
+
+#ubuntu
+apt-get install -y php-pear php-dev libevent-dev
+printf "\n\n /usr/lib/x86_64-linux-gnu/\n\n\nno\n\n\n" | pecl install event > /dev/null
+echo "extension=event.so" > /etc/php/7.3/cli/conf.d/event.ini
