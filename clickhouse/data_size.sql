176c3c8fec4885c72e846891ce0e125ec3b66a23
Vladimir Goncharov <morozovsk@gmail.com>
2020-04-09 02:16:35 +0300

    Update data_size.sql

--- clickhouse/data_size.sql
+++ clickhouse/data_size.sql
@@ -1,3 +1,16 @@
+select concat(database, '.', table)                         as table,
+       formatReadableSize(sum(bytes))                       as size,
+       sum(rows)                                            as rows,
+       max(modification_time)                               as latest_modification,
+       sum(bytes)                                           as bytes_size,
+       any(engine)                                          as engine,
+       formatReadableSize(sum(primary_key_bytes_in_memory)) as primary_keys_size,
+       formatReadableSize(sum(bytes) / rows * 1000000)      as size_per_1kk_rows
+from system.parts
+where active
+group by database, table
+order by bytes_size desc;
+
 -- by columns:
 SELECT
     table, name,
